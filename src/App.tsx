ac5a196840879150427c48e4c9af03679425cc06
Nkaka23dev <nkakaeric96@gmail.com>
2023-04-16 15:32:05 +0200

    feat: add state code for react nested routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import About from "./pages/About";
 import NavBar from "./components/layout/NavBar";
 import Footer from "./components/layout/Footer";
-import Vans from "./pages/Vans";
 import { QueryClient, QueryClientProvider } from 'react-query';
-import VanDetails from "./pages/VanDetails";
 
 const queryClient = new QueryClient();
 
 export default function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <Router>
-        <NavBar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="about/" element={<About />} />
-          <Route path="vans/" element={<Vans />} />
-          <Route path="vans/:id/" element={<VanDetails />} />
-        </Routes>
-        <Footer />
-      </Router>
-    </QueryClientProvider>
+      <NavBar/>
+     </QueryClientProvider>
   )
 }
--- src/components/layout/NavBar.tsx
+++ src/components/layout/NavBar.tsx
@@ -1,15 +1,15 @@
-import { Link } from "react-router-dom";
+
 
 export default function NavBar() {
   return (
     <section className="bg-[#FFF7ED] px-5 fixed w-full left-0 right-0 top-0 ">
-      <div className="max-w-6xl mx-auto py-8 flex items-center justify-between text-2xl">
-        <Link to="/" className="font-bold cursor-pointer text-3xl">#VANALIFE</Link>
-        <div className="flex font-semibold items-center cursor-pointer gap-10">
-          <Link to="/about" className="hover:underline">  About</Link>
-          <Link to="/vans" className="hover:underline">Vans</Link>
-        </div>
-      </div>
+      <ul className="max-w-6xl mx-auto py-8 flex items-center justify-between text-2xl">
+        <li  className="font-bold cursor-pointer text-3xl">#VANALIFE</li>
+        <ul className="flex font-semibold items-center cursor-pointer gap-10">
+          <li className="hover:underline">  About</li>
+          <li  className="hover:underline">Vans</li>
+        </ul>
+      </ul>
     </section>
   );
 }
