ef2c0be0d0c27c088e327fd767b6e6183633c6dc
Nkaka23dev <nkakaeric96@gmail.com>
2023-04-16 15:41:51 +0200

    feat: modified Navbar

--- src/components/layout/NavBar.tsx
+++ src/components/layout/NavBar.tsx
@@ -6,7 +6,8 @@ export default function NavBar() {
       <ul className="max-w-6xl mx-auto py-8 flex items-center justify-between text-2xl">
         <li  className="font-bold cursor-pointer text-3xl">#VANALIFE</li>
         <ul className="flex font-semibold items-center cursor-pointer gap-10">
-          <li className="hover:underline">  About</li>
+        <li className="hover:underline">Host</li>
+          <li className="hover:underline">About</li>
           <li  className="hover:underline">Vans</li>
         </ul>
       </ul>
